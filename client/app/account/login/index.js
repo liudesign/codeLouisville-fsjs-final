b73e5db27344517e76951b212efb830812224bbe
john.liu <john.liu@cdiniteractive.com>
2016-11-17 23:42:27 -0500

    inital scaffolding files angular-fullstack

---
+++ client/app/account/login/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import LoginController from './login.controller';
+
+export default angular.module('codeLouisvilleFsjsFinalApp.login', [])
+  .controller('LoginController', LoginController)
+  .name;
